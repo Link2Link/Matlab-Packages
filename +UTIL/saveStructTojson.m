39555c0e4d374d74bd8edb8772bf5aa716ddc1c2
WANG CHENFEI <barrycf.wang@gmail.com>
2023-04-26 10:22:19 +0800

    改善json可读性

--- +UTIL/saveStructTojson.m
+++ +UTIL/saveStructTojson.m
@@ -1,6 +1,6 @@
 function saveStructTojson(file, data)
 %saveStruct2json 保存结构体到json文件
-txt = jsonencode(data);
+txt = jsonencode(data, "PrettyPrint", true);
 fid = fopen(file,'w');
 fprintf(fid, txt);
 fclose(fid);
